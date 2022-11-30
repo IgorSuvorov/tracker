03136ecf72ee0e2713d4de207dab177c0655458c
isuvorov1 <isuvorov1@outlook.com>
2022-11-29 22:14:59 -0500

    validate input test

--- src/test/java/TrackerTest.java
+++ src/test/java/TrackerTest.java
@@ -268,4 +268,26 @@ public class TrackerTest {
                         + "=== Exit the application ===" + ln
         );
     }
+
+    @Test
+    public void whenInvalidExit() {
+        Output out = new StubOutput();
+        Input in = new StubInput(
+                new String[] {"5", "0"}
+        );
+        Tracker tracker = new Tracker();
+        UserAction[] actions = new UserAction[]{
+                new ExitAction(out)
+        };
+        new StartUI(out).init(in, tracker, actions);
+        String ln = System.lineSeparator();
+        assertThat(out.toString()).isEqualTo(
+                        "Menu:" + ln
+                        + "0. Exit the application" + ln
+                        + "Wrong input, you can select: 0 .. 0" + ln
+                        + "Menu:" + ln
+                        + "0. Exit the application" + ln
+                        + "=== Exit the application ===" + ln
+        );
+    }
 }
