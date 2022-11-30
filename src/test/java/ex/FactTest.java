820050271db06b81860bf4ff6e296420e8df20ba
isuvorov1 <isuvorov1@outlook.com>
2022-11-29 21:21:40 -0500

    factorial test

---
+++ src/test/java/ex/FactTest.java
@@ -0,0 +1,17 @@
+package ex;
+
+import org.junit.jupiter.api.Test;
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
+public class FactTest {
+    @Test
+    public void whenNIsLessThanZeroThenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Fact().calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
+    }
+}
