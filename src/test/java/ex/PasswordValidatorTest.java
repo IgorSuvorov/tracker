a3b8e8bdf52665784ba6361871f32d35245a01f6
isuvorov1 <isuvorov1@outlook.com>
2022-12-01 17:20:43 -0500

    fixed

--- src/test/java/ex/PasswordValidatorTest.java
+++ src/test/java/ex/PasswordValidatorTest.java
@@ -86,7 +86,7 @@ class PasswordValidatorTest {
                 IllegalArgumentException.class,
                 () -> PasswordValidator.validate(password)
         );
-        String expected = "Password should contain at least one special character";
+        String expected = "Password should contain at least one special symbol";
         assertThat(exception.getMessage()).isEqualTo(expected);
     }
 
