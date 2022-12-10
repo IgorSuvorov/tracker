f7a13b71e70f3263a9225a82291351b1bfbdfe74
isuvorov1 <isuvorov1@outlook.com>
2022-12-10 17:19:31 -0500

    6. Список адресов [#110062 #369545]

--- src/main/java/ru/job4j/stream/Profiles.java
+++ src/main/java/ru/job4j/stream/Profiles.java
@@ -3,13 +3,14 @@ package ru.job4j.stream;
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 public class Profiles {
 
     public static List<Address> collect(List<Profile> profiles) {
         return profiles.stream()
-                .map(profile -> profile.getAddress())
+                .map(Profile::getAddress)
                 .collect(Collectors.toList());
     }
 
